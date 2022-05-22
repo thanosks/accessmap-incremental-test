406d6f29c74a556fdfb2394ab7ed3dc0ac4398f3
Debian <azureuser@incremental.rfpojoxvjuuelkxedhodtv1rue.dx.internal.cloudapp.net>
2022-05-22 03:28:29 +0000

    Add Columbus, OH, include tasks dir, and make rebuild.sh do a release

--- rebuild.sh
+++ rebuild.sh
@@ -7,3 +7,5 @@ set -e
 docker-compose run --rm data_osm_osw
 docker-compose run --rm data_incremental
 docker-compose run --rm build_router & docker-compose run --rm build_tiles
+docker-compose run --rm build_webapp
+docker-compose restart caddy
